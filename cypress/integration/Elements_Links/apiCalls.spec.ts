d55d9f3d3d34f4ea1d2e8b157998ea1fd902925a
markjobenjoco <markjobenjoco@gmail.com>
2021-11-01 10:17:03 +0800

    modified links

--- cypress/integration/Elements_Links/apiCalls.spec.ts
+++ cypress/integration/Elements_Links/apiCalls.spec.ts
@@ -2,22 +2,28 @@ import { Then, When } from 'cypress-cucumber-preprocessor/steps'
 import { links } from '..//..//support/page-objects/elements_po'
 
 When('I click {string}', (link: string) => {
-  links.getAPILink(link).then(($link) => {
-    cy.wrap($link)
-      .invoke('attr', 'id')
-      .then((val) => cy.intercept(`/${val}`).as('req'))
-    cy.wrap($link).click()
-  })
+  interceptRequest(link).as('req')
+  links.getAPILink(link).click()
 })
 Then('Should display the request output below', () => {
   cy.wait('@req')
     .then((xhr) => {
-      const actual = { statCode: xhr.response?.statusCode, message: xhr.response?.statusMessage }
-      return actual
+      const res = { statCode: xhr.response?.statusCode, message: xhr.response?.statusMessage }
+      return res
     })
-    .then((actual) => {
-      cy.wrap(actual).then((value) => {
+    .then((res) => {
+      cy.wrap(res).then((value) => {
         links.returnOutput().should('contain', value.statCode).and('contain', value.message)
       })
     })
 })
+
+const interceptRequest = (link: string) => {
+  return links.getAPILink(link).then(($link) => {
+    cy.wrap($link)
+      .invoke('attr', 'id')
+      .then((val) => {
+        return cy.intercept(`/${val}`)
+      })
+  })
+}
